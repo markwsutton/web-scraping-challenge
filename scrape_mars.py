25b5959d4a8fed2a9e26b3b98630ed757be6aa4d
markwsutton <64506426+markwsutton@users.noreply.github.com>
2020-08-10 19:37:47 -0700

    app and scrape_mars initial scripts
    undebugged code

---
+++ scrape_mars.py
@@ -0,0 +1,24 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape():
+    browser = init_browser()
+    listings = {}
+
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    listings["news_title"] = soup.find("div", class_="bottom_gradient").get_text()
+    listings["news_p"] = soup.find("div", class_="article_teaser_body").get_text()
+
+    return listings
